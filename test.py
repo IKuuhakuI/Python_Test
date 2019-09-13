a1138afce5812d9fcb48d9d6134c5a2abeeb9262
Luiz Carlos Aguiar <luizcarlosaguiarc@gmail.com>
2019-09-13 20:58:12 -0300

    Connect to Firebase

---
+++ test.py
@@ -0,0 +1,13 @@
+import pyrebase
+
+config = {
+    "apiKey": "AIzaSyBrarBhWJSP3FnNJurEAtrbmUb1fG_wZFs",
+    "authDomain": "teste-python-67d43.firebaseapp.com",
+    "databaseURL": "https://teste-python-67d43.firebaseio.com",
+    "projectId": "teste-python-67d43",
+    "storageBucket": "",
+    "messagingSenderId": "581051665954",
+    "appId": "1:581051665954:web:6f131448200a100689447b"
+  }
+
+  firebase = pyrebase.initialize_app(config)
