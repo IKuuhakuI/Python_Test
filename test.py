4dc58a3ac682703fa24581b4c5779027b26d4163
Luiz Carlos Aguiar <luizcarlosaguiarc@gmail.com>
2019-09-13 21:31:26 -0300

    Print a random question number from the database

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import pyrebase
+import random
 
+#Configuracoes importantes para linkar ao projeto do Firebase
 config = {
     "apiKey": "AIzaSyBrarBhWJSP3FnNJurEAtrbmUb1fG_wZFs",
     "authDomain": "teste-python-67d43.firebaseapp.com",
@@ -10,15 +12,22 @@ config = {
     "appId": "1:581051665954:web:6f131448200a100689447b"
 }
 
+#Faz conexao com Firebase
 firebase = pyrebase.initialize_app(config)
 
+#Faz a conexao com o banco de dados externo
 db = firebase.database()
 
+#Variavel que recebe todos os dados do no principal
 dados = db.get()
 
-num = 0
+#Conta quantas perguntas existem no banco de dados
+quantidadePeguntas = 0
 
-for dado in dados.each():
-    num += 1
+#Para cada pergunta, incrementa o valor em 1
+for perguntas in dados.each():
+    quantidadePeguntas += 1
 
-print(num)
+perguntaAtual = random.randrange(1, quantidadePeguntas+1)
+
+print(perguntaAtual)
